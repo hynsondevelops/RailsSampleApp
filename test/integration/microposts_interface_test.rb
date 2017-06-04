cff72ab4b3fd1d9b148c96d2bf0cf34708da0ca5
Adam Hynson <hynsondevelops@gmail.com>
2017-06-04 12:07:36 -0400

    Add user microposts

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    # Invalid submission
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "", picture: picture } }
+    end
+    assert_select 'div#error_explanation'
+    # Delete post
+    assert_select 'a', text: 'sample app'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # Visit different user (no delete links)
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+  
+  
+end
+ 
